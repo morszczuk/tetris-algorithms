0dd9b062a2bf17602f6242abbc68a1638b82a704
Miron Marczuk <miron.marczuk@gmail.com>
2016-10-24 02:46:52 +0400

    Project ViewModel added to solution, basic responses implemented

---
+++ Tetris/Tetris.ViewModel/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Tetris.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private bool _isAlgorithmActive = false;
+        private bool _areComputationsRunning = false;
+
+        public bool IsAlgorithmActive
+        {
+            get
+            { 
+                return _isAlgorithmActive;
+            }
+        }
+
+        public bool IsAlgorithmLaunchWindowShowed
+        {
+            get
+            {
+                return !_isAlgorithmActive;
+            }
+        }
+
+        public bool IsRunningAlgorithmWindowShowed
+        {
+            get
+            {
+                return _isAlgorithmActive;
+            }
+        }
+
+        public bool AreComputationsRunning
+        {
+            get
+            {
+                return _areComputationsRunning;
+            }
+        }
+
+        public bool AreComputationsPaused
+        {
+            get
+            {
+                return !_areComputationsRunning;
+            }
+            set
+            {
+                _areComputationsRunning = !value;
+                OnPropertyChanged("AreComputationsRunning");
+                OnPropertyChanged("AreComputationsPaused");
+                OnPropertyChanged("ComputationsButtonComunicate");
+            }
+        }
+
+        public string ComputationsButtonComunicate
+        {
+            get
+            {
+                return _areComputationsRunning ? "STOP" : "KONTYNUUJ";
+            }
+        }
+
+        public ICommand StartComputationsCommand
+        {
+            get { return new RelayCommand((x) => this.StartComputations(x)); }
+        }
+
+        public void StartComputations(object parameter)
+        {
+            _isAlgorithmActive = true;
+            _areComputationsRunning = true;
+
+            OnPropertyChanged("IsAlgorithmLaunchWindowShowed");
+            OnPropertyChanged("IsRunningAlgorithmWindowShowed");
+            OnPropertyChanged("AreComputationsRunning");
+            OnPropertyChanged("AreComputationsPaused");
+            OnPropertyChanged("ComputationsButtonComunicate");
+            OnPropertyChanged("IsAlgorithmActive");
+        }
+
+        public ICommand ChangeComputationsStatusCommand
+        {
+            get { return new RelayCommand((x) => this.ChangeComputationsStatus(x)); }
+        }
+        
+        void ChangeComputationsStatus(object parameter)
+        {
+            _areComputationsRunning = !_areComputationsRunning;
+            OnPropertyChanged("AreComputationsRunning");
+            OnPropertyChanged("AreComputationsPaused");
+            OnPropertyChanged("ComputationsButtonComunicate");
+        }
+
+        public ICommand EndComputationsCommand
+        {
+            get { return new RelayCommand((x) => this.EndComputations(x)); }
+        }
+
+        void EndComputations(object parameter)
+        {
+            _isAlgorithmActive = false;
+            OnPropertyChanged("IsAlgorithmActive");
+            OnPropertyChanged("IsAlgorithmLaunchWindowShowed");
+            OnPropertyChanged("IsRunningAlgorithmWindowShowed");
+        }
+        
+    }
+}
---
+++ Tetris/Tetris.ViewModel/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public ViewModelBase()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal virtual void OnPropertyChanged([CallerMemberName] string caller = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+    }
+}
