0662a22a4d765bb6ca2d4803a615b9082aa0126e
Patryk Pastewski <past.patryk@gmail.com>
2016-11-17 20:28:34 +0100

    Added empty unit tests project

---
+++ Tetris/TetrisTests/AlgorithmTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tetris.AlgorithmLogic;
+using Tetris.Enums;
+using Tetris.Helpers;
+using Tetris.Models;
+
+namespace TetrisTests
+{
+    [TestClass]
+    public class AlgorithmTest
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
