4bf520d368894acc2a05e7526d6e48560612be92
mkwie <matkwiecinski@gmail.com>
2016-11-18 21:18:31 +0100

    well visualization v2

--- Tetris/Tetris/Converters/WellStateToVisual.cs
+++ Tetris/Tetris/Converters/WellStateToVisual.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -10,45 +11,52 @@ using System.Windows.Data;
 using System.Windows.Ink;
 using System.Windows.Media;
 using Tetris.Models;
+using Color = System.Windows.Media.Color;
+using System.Drawing;
 
 namespace Tetris.Converters
 {
     class WellStateToVisual : IValueConverter
     {
+
+        private readonly int _colorStep = 10;
+        
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+
             var item = value as WellState;
 
-
-
-            //var partWidth = (500) / item.Well.Width;
-            //var partHeight = (500) / item.Fill.Count;
-
-            //var parts = new List<System.Windows.Shapes.Rectangle>();
             var buttons = new List<Button>();
+            int colorCounter = 0;
 
-            for (var y = item.Fill.Count - 1; y >= 0; y--)
+            for (int i = 0; i < item.Bricks.Count; i++)
             {
-                for (var x = 0; x < item.Well.Width; x++)
+
+                var brick = item.Bricks[i].Brick;
+                if (_colorStep * colorCounter > 255) colorCounter -= i;
+                byte c = (byte)(_colorStep * colorCounter);
+                var colour = Color.FromArgb(255, c, c, c);
+                for (int j = 0; j < brick.Height; j++)
                 {
-                    if (item.Fill[y][x])
+                    for (int k = 0; k < brick.Width; k++)
                     {
-
-                        Button b = new Button()
+                        if (brick.Body[j, k])
                         {
-                            Background = new SolidColorBrush(Colors.Black),
-                            HorizontalAlignment = HorizontalAlignment.Stretch,
-                            VerticalAlignment = VerticalAlignment.Center,
-                        };
-
-                        Grid.SetRow(b,item.Fill.Count -1 -  y);
-                        Grid.SetColumn(b, x);
-                        buttons.Add(b);
+                            Button b = new Button()
+                            {
+                                Background = new SolidColorBrush(colour)
+                            };
+                            Grid.SetRow(b, item.Fill.Count - 1 - item.Bricks[i].Y + j);
+                            Grid.SetColumn(b, item.Bricks[i].X + k);
+                            buttons.Add(b);
+                        }   
                     }
                 }
+                colorCounter++;
             }
             return buttons;
-            
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
