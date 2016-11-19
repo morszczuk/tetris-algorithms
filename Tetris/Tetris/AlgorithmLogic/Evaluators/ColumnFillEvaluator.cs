834cdd5cedc7ab53df1bbce674a43ac37ad0753e
Patryk Pastewski <past.patryk@gmail.com>
2016-11-19 19:16:16 +0100

    Added ColumnFillEvaluator

---
+++ Tetris/Tetris/AlgorithmLogic/Evaluators/ColumnFillEvaluator.cs
@@ -0,0 +1,24 @@
+﻿using Tetris.Models;
+
+namespace Tetris.AlgorithmLogic.Evaluators
+{
+    public class ColumnFillEvaluator : IWellStateEvaluator
+    {
+        public int Evaluate(WellState wellState)
+        {
+            var value = wellState.Well.Width * wellState.Fill.Count;
+            for (var x = 0; x < wellState.Well.Width; x++)
+            {
+                for (var y = wellState.Fill.Count - 1; y >= 0; y--)
+                {
+                    if (wellState.Fill[y][x])
+                    {
+                        value += y;
+                        break;
+                    }
+                }
+            }
+            return -value;
+        }
+    }
+}
